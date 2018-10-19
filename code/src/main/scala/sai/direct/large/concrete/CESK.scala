b96978351f7e1bc4750046b723f17736ea2add9f
Yuxuan Chen <ychen@yuxuanlabs.com>
2018-10-19 02:48:10 -0400

    Implement more CES concrete interpreter

--- code/src/main/scala/sai/direct/large/concrete/CESK.scala
+++ code/src/main/scala/sai/direct/large/concrete/CESK.scala
@@ -15,6 +15,7 @@ object CESK {
   case class ListV(l: List[Value]) extends Value with Expr
   case class VectorV(v: Vector[Value]) extends Value with Expr
   case class CloV(λ: Lam, ρ: Env) extends Value
+  case class VoidV() extends Value
 
   abstract class Kont
   object Halt extends Kont
@@ -42,12 +43,61 @@ import CESK._
 
 object BigStepCES {
 
+  // evaluates a list of Exprs in order
+  // returns List of evaluated values and an ending state
+  def interpListOfExprs(l: List[Expr], env:Env, sigma: Store): (List[Value], Store) = l match {
+    case Nil => (Nil, sigma)
+    case e::el =>
+      val (ev, es) = interp(e, env, sigma)
+      val (elv, els) = interpListOfExprs(el, env, es)
+      (ev :: elv, els)
+  }
+
+  def interpSeq(l: List[Expr], env: Env, sigma: Store): (Value, Store) = l match {
+    case Nil => (VoidV(), sigma)
+    case e::Nil => interp(e, env, sigma)
+    case e::el =>
+      val (_, es) = interp(e, env, sigma)
+      interpSeq(el, env, es)
+  }
+
   def interp(e: Expr, env: Env, sigma: Store): (Value, Store) = e match {
     case IntLit(x) => (NumV(x), sigma)
     case BoolLit(x) => (BoolV(x), sigma)
     case CharLit(x) => (CharV(x), sigma)
     case Var(x) => (sigma(env(x)), sigma)
     case l@Lam(_, _) => (CloV(l, env), sigma)
+    case If(cnd, thn, els) =>
+      val (ev, es) = interp(cnd, env, sigma)
+      ev match {
+        case BoolV(true) => interp(thn, env, es)
+        case BoolV(false) => interp(els, env, es)
+      }
+
+    case Cond(branches) => ???
+    case Case(e, branches) => ???
+    case App(e, param) =>
+      val (ev, es) = interp(e, env, sigma)
+      val (appv, es_) = interpListOfExprs(param, env, es)
+      val addrs = appv map { (v: Value) => alloc(es_) }
+      ev match {
+        case CloV(Lam(args, lbody), cenv) =>
+          interp(lbody, cenv ++ (args zip addrs), es_ ++ (addrs zip appv))
+      }
+
+    case Void() => (VoidV(), sigma)
+    case Set_!(x, e) =>
+      val (ev, es) = interp(e, env, sigma)
+      val toAdd = (env(x), ev)
+      (VoidV(), es + toAdd)
+    case Begin(l) => interpSeq(l, env, sigma)
+    case Define(x: String, e: Expr) => ???
   }
   def eval(e: Expr): (Value, Store) = interp(e, Map(), Map())
+}
+
+object CESKTest {
+  def main(args: Array[String]) = {
+    assert(BigStepCES.eval(IntLit(1)) == (NumV(1), Map()))
+  }
 }
