177068809d0391d03c035a76130158e62c338089
Yuxuan Chen <ychen@yuxuanlabs.com>
2018-10-23 20:42:57 -0400

    Make it compile

--- code/src/main/scala/sai/direct/large/parser/Transform.scala
+++ code/src/main/scala/sai/direct/large/parser/Transform.scala
@@ -12,30 +12,32 @@ object LargeSchemeASTDesugar {
     App(Lam(List(newIdent), f(Var(newIdent))), List(Var(newIdent)))
   }
 
-  def desugarCondBranches(branches: List[CondBrTrait]) = branches match {
-    case Nil => Void()
-    case x :: xs =>
-      x match {
-        case CondBr(cnd, thn) =>
-          If(apply(cnd), apply(thn), desugarCondBranches(xs))
-        case CondProcBr(cnd, thnl) =>
-          newIdentLet(apply(cnd)) {
-            v => If(v, App(apply(thn), List(v)), desugarCondBranches(xs))
-          }
-      }
-  }
+  def desugarCondBranches(branches: List[CondBrTrait]): Expr = 
+    branches match {
+      case Nil => Void()
+      case x :: xs =>
+        x match {
+          case CondBr(cnd, thn) =>
+            If(apply(cnd), apply(thn), desugarCondBranches(xs))
+          case CondProcBr(cnd, thnl) =>
+            newIdentLet(apply(cnd)) {
+              v => If(v, App(apply(thnl), List(v)), desugarCondBranches(xs))
+            }
+        }
+    }
 
-  def desugarCaseBranches(comp: Expr, branches: List[CaseBranch]) = branches match {
-    case Nil => Void()
-    case x :: xs =>
-      val CaseBranch(cases, thn) = x
-      val xsCases = desugarCaseBranches(xs)
-      cases.foldRight(xsCases) {
-        case (e, xsCases) => If(App("eq?", List(comp, e)), thn, xsCases)
-      }
-  }
+  def desugarCaseBranches(comp: Expr, branches: List[CaseBranch]): Expr = 
+    branches match {
+      case Nil => Void()
+      case x :: xs =>
+        val CaseBranch(cases, thn) = x
+        val xsCases = desugarCaseBranches(comp, xs)
+        cases.foldRight(xsCases) {
+          case (e, xsCases) => If(App(Var("eq?"), List(comp, e)), thn, xsCases)
+        }
+    }
 
-  def apply(expr: Expr) = expr match {
+  def apply(expr: Expr): Expr = expr match {
     case App(e1, param) => App(apply(e1), param map apply)
     case Lam(param, body) => Lam(param, apply(body))
     case If(cnd, thn, els) => If(apply(cnd), apply(thn), apply(els))
