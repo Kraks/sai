9c02b2f34796ad1f751d029068f6fc24dc93922d
David Deng <daviddenghaotian@163.com>
2021-12-15 01:46:17 -0500

    fixed init_bloop script

--- docker-image/init_bloop.sh
+++ docker-image/init_bloop.sh
@@ -4,7 +4,8 @@ cd ../dev-clean
 curl -fLo coursier https://git.io/coursier-cli &&
 	chmod +x coursier &&
 	./coursier install bloop --only-prebuilt=true &&
-	export PATH="$PATH:/root/.local/share/coursier/bin" && 
 	echo 'addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "1.4.11")' | tee -a project/plugins.sbt ../lms-clean/project/plugins.sbt &&
 	sbt bloopInstall
 rm coursier
+# to make bloop available, do 
+# export PATH="$PATH:/root/.local/share/coursier/bin"
