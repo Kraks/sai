0b8cd38e89684ad62fec59742ea5b71dd697653b
Guannan Wei <guannanwei@outlook.com>
2022-02-08 02:55:05 -0500

    refactor external_pure

--- dev-clean/headers/llsc/external_pure.hpp
+++ dev-clean/headers/llsc/external_pure.hpp
@@ -1,6 +1,9 @@
 #ifndef LLSC_EXTERNAL_PURE_HEADERS
 #define LLSC_EXTERNAL_PURE_HEADERS
 
+template<typename T> using __Cont = std::function<T(SS, PtrVal)>;
+template<typename T> using __Halt = std::function<T(SS, immer::flex_vector<PtrVal>)>;
+using ListSSVal = immer::flex_vector<std::pair<SS, PtrVal>>;
 using Cont = std::function<std::monostate(SS, PtrVal)>;
 
 inline std::string get_string(PtrVal ptr, SS state) {
@@ -111,24 +114,8 @@ inline immer::flex_vector<std::pair<SS, PtrVal>> realloc(SS state, immer::flex_v
   return immer::flex_vector<std::pair<SS, PtrVal>>{{res, memLoc}};
 }
 
-inline immer::flex_vector<std::pair<SS, PtrVal>> sym_exit(SS state, immer::flex_vector<PtrVal> args) {
-  ASSERT(args.size() == 1, "sym_exit accepts exactly one argument");
-  auto v = args.at(0)->to_IntV();
-  ASSERT(v != nullptr, "sym_exit only accepts integer argument");
-  int status = v->as_signed();
-  check_pc_to_file(state);
-#ifdef USE_TP
-  tp.stop_all_tasks();
-  set_exit_code(status);
-  return immer::flex_vector<std::pair<SS, PtrVal>>{};
-#else
-  cov.print_all();
-  _exit(status);
-#endif
-}
-
-/* TODO: Generate both versions of sym_exit <2022-01-24, David Deng> */
-inline std::monostate sym_exit(SS state, immer::flex_vector<PtrVal> args, Cont k) {
+template<typename T>
+inline T __sym_exit(SS& state, immer::flex_vector<PtrVal>& args, __Cont<T> k) {
   ASSERT(args.size() == 1, "sym_exit accepts exactly one argument");
   auto v = args.at(0)->to_IntV();
   ASSERT(v != nullptr, "sym_exit only accepts integer argument");
@@ -138,41 +125,47 @@ inline std::monostate sym_exit(SS state, immer::flex_vector<PtrVal> args, Cont k
   // XXX: brutally call _exit? then what should happen if two threads are calling sym_exit?
   tp.stop_all_tasks();
   set_exit_code(status);
-  return std::monostate{};
+  return k(state, nullptr);
 #else
   cov.print_all();
   _exit(status);
 #endif
 }
 
-inline immer::flex_vector<std::pair<SS, PtrVal>> llsc_assert(SS state, immer::flex_vector<PtrVal> args) {
-  auto v = args.at(0);
-  auto i = v->to_IntV();
-  if (i) {
-    if (i->i == 0) return stop(state, args); // concrete false - generate the test and exit
-    return immer::flex_vector<std::pair<SS, PtrVal>>{{state, make_IntV(1, 32)}};
-  }
-  // otherwise add a symbolic condition that constraints it to be true
-  // undefined/error if v is a value of other types
-  auto cond = to_SMTNeg(v);
-  auto new_s = state.add_PC(cond);
-  if (check_pc(new_s.get_PC())) return stop(new_s, args); // check if v == 1 is not valid
-  return immer::flex_vector<std::pair<SS, PtrVal>>{{state.add_PC(v), make_IntV(1, 32)}};
+inline immer::flex_vector<std::pair<SS, PtrVal>> sym_exit(SS state, immer::flex_vector<PtrVal> args) {
+  return __sym_exit<ListSSVal>(state, args, [](auto s, auto v) { return immer::flex_vector<std::pair<SS, PtrVal>>{}; });
 }
 
-inline std::monostate llsc_assert(SS state, immer::flex_vector<PtrVal> args, Cont k) {
+inline std::monostate sym_exit(SS state, immer::flex_vector<PtrVal> args, Cont k) {
+  return __sym_exit<std::monostate>(state, args, [](auto s, auto v) { return std::monostate{}; });
+}
+
+template<typename T>
+inline T __llsc_assert(SS& state, immer::flex_vector<PtrVal>& args, __Cont<T> k, __Halt<T> h) {
   auto v = args.at(0);
   auto i = v->to_IntV();
   if (i) {
-    if (i->i == 0) return stop(state, args, k); // concrete false - generate the test and exit
+    if (i->i == 0) return h(state, args); // concrete false - generate the test and ``halt''
     return k(state, make_IntV(1, 32));
   }
   // otherwise add a symbolic condition that constraints it to be true
   // undefined/error if v is a value of other types
   auto cond = to_SMTNeg(v);
   auto new_s = state.add_PC(cond);
-  if (check_pc(new_s.get_PC())) return stop(new_s, args, k); // check if v == 1 is not valid
+  if (check_pc(new_s.get_PC())) return h(new_s, args); // check if v == 1 is not valid
   return k(state.add_PC(v), make_IntV(1, 32));
+}
+
+inline immer::flex_vector<std::pair<SS, PtrVal>> llsc_assert(SS state, immer::flex_vector<PtrVal> args) {
+  return __llsc_assert<ListSSVal>(state, args,
+      [](auto s, auto v) { return immer::flex_vector<std::pair<SS, PtrVal>>{{s, v}}; },
+      [](auto s, auto a) { return stop(s, a); });
+}
+
+inline std::monostate llsc_assert(SS state, immer::flex_vector<PtrVal> args, Cont k) {
+  return __llsc_assert<std::monostate>(state, args,
+      [&k](auto s, auto v) { return k(s, v); },
+      [&k](auto s, auto a) { return stop(s, a, k); });
 }
 
 inline immer::flex_vector<std::pair<SS, PtrVal>> llsc_assert_eager(SS state, immer::flex_vector<PtrVal> args) {
