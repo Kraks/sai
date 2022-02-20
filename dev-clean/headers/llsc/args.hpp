fe9351c7c478e48aac02934f62334fe8eff7849f
David Deng <daviddenghaotian@163.com>
2022-02-20 18:24:53 -0500

    implemented MatchResult and removed ArgSeg

--- dev-clean/headers/llsc/args.hpp
+++ dev-clean/headers/llsc/args.hpp
@@ -10,110 +10,25 @@ inline immer::flex_vector<immer::flex_vector<PtrVal>> cli_argv;
 inline PtrVal g_argv;
 inline PtrVal g_argc;
 
-// for testing and storing intermediate parsing result.
-class ArgSeg {
-  public:
-    immer::flex_vector<PtrVal> to_flex_vector() const {
-      if (next_segment == nullptr) {
-        return _to_flex_vector();
-      } else {
-        return _to_flex_vector() + next_segment->to_flex_vector();
-      }
-    }
-    bool operator==(const ArgSeg& other) const {
-      return typeid(*this) == typeid(other) && equal(other) && (
-          (other.next_segment == nullptr && 
-           next_segment == nullptr) ||
-          *next_segment == *(other.next_segment)
-          );
-    };
-    ArgSeg *set_next(ArgSeg *next) {
-      if (next_segment != nullptr) {
-        delete next_segment;
-      }
-      next_segment = next;
-      return next_segment;
-    }
-    virtual immer::flex_vector<PtrVal> _to_flex_vector() const = 0;
-    virtual std::string toString() const = 0;
-    virtual bool equal(const ArgSeg&) const = 0;
-    friend std::ostream& operator<<(std::ostream& os, const ArgSeg& a) {
-      os << a.toString();
-      if (a.next_segment != nullptr) {
-        os << " -> ";
-        os << *a.next_segment;
-      }
-      return os;
-    }
-    ArgSeg(ArgSeg* next): next_segment(next) {}
-    virtual ~ArgSeg() {
-      if (next_segment != nullptr) {
-        delete next_segment;
-      }
-    }
-    ArgSeg *next_segment;
-};
+using MatchResult = List<std::variant<unsigned, std::string>>;
 
-class ConcSeg: public ArgSeg {
-  public:
-    ConcSeg (const std::string &content, ArgSeg* next = nullptr): content(content), ArgSeg(next) {};
-    std::string toString() const override {
-      return std::string("[Conc: ") + content + "]";
-    }
-    immer::flex_vector<PtrVal> _to_flex_vector() const override {
-      immer::flex_vector<PtrVal> vec;
+List<PtrVal> match_result_to_ptrvals(MatchResult mr) {
+  List<PtrVal> vec;
+  for (auto segment: mr) {
+    if (std::holds_alternative<unsigned>(segment)) {
+      auto length = std::get<unsigned>(segment);
+      for (auto i = 0; i < length; ++i) {
+        vec = vec.push_back(make_SymV_args());
+      }
+    } else if (std::holds_alternative<std::string>(segment)) {
+      auto content = std::get<std::string>(segment);
       for (auto c: content) {
         vec = vec.push_back(make_IntV(c, 8));
       }
-      return vec;
-    };
-    bool equal(const ArgSeg& other) const override {
-      return static_cast<const ConcSeg&>(other).content == content;
     }
-    ~ConcSeg() {
-#ifdef DEBUG
-      std::cout << "~ConcSeg() called" << std::endl;
-#endif
-    }
-  private:
-    std::string content;
-};
-
-class SymSeg: public ArgSeg {
-  public:
-    SymSeg (unsigned length, ArgSeg* next = nullptr): length(length), ArgSeg(next) {};
-    std::string toString() const override {
-      return std::string("[Sym: ") + std::to_string(length) + "]";
-    }
-    immer::flex_vector<PtrVal> _to_flex_vector() const override {
-      immer::flex_vector<PtrVal> vec;
-      for (unsigned i = 0; i < length; ++i) {
-        vec = vec.push_back(make_SymV_args());
-      }
-      return vec;
-    };
-    bool equal(const ArgSeg& other) const override {
-      return static_cast<const SymSeg&>(other).length == length;
-    }
-    ~SymSeg() {
-#ifdef DEBUG
-      std::cout << "~SymSeg() called" << std::endl;
-#endif
-    }
-  private:
-    unsigned length;
-};
-
-/* int main() { */
-/*   auto arg1 = ConcSeg("hello", new ConcSeg("second", new SymSeg(43))); */
-/*   auto arg2 = ConcSeg("hello", new ConcSeg("second", new SymSeg(43))); */ 
-/*   auto arg3 = ConcSeg("hello", new ConcSeg("third", new SymSeg(43))); */ 
-/*   std::cout << "ArgSeg == arg2: " << (arg1 == arg2) << std::endl; */
-/*   std::cout << "ArgSeg == arg2: " << (arg1 == arg3) << std::endl; */
-/*   std::cout << "arg1: " << arg1 << std::endl; */
-/*   std::cout << "arg2: " << arg2 << std::endl; */
-/*   std::cout << "arg3: " << arg3 << std::endl; */
-/* } */
+  }
+  return vec;
+}
 
 inline std::string escape_concrete(const std::string &raw) {
    auto content = raw;
@@ -122,7 +37,7 @@ inline std::string escape_concrete(const std::string &raw) {
    return content;
 }
 
-inline std::shared_ptr<ArgSeg> match_arg(std::string argstring) {
+inline MatchResult match_arg(std::string argstring) {
   // the regex matching function
   std::smatch sm;
   std::string symarg (R"(#\{?([[:d:]]+)\}?)");
@@ -136,7 +51,6 @@ inline std::shared_ptr<ArgSeg> match_arg(std::string argstring) {
   std::string conc_content; // store the concrete content
   int sym_length; // store the symbolic length
 
-
   /*
    * input            conc_contents       | sym_contents
    * abc    -->       "abc"               | _
@@ -148,24 +62,20 @@ inline std::shared_ptr<ArgSeg> match_arg(std::string argstring) {
    * abc#13def -->    "abc" "def"         | 13
    */
   
-  ArgSeg *node, *head;
-  ConcSeg dummy(""); // first dummy node
-  head = node = &dummy;
+  MatchResult result;
   while (conc_contents!=rend) {
     conc_content = escape_concrete(conc_contents->str());
     if (!conc_content.empty()) {
-      node = node->set_next(new ConcSeg(conc_content));
+      result = result.push_back({conc_content});
     }
     conc_contents++;
     if (sym_lengths != rend) {
       /* TODO: set a bound on sym_length <2022-02-14, David Deng> */
       sym_length = stoi(sym_lengths->str());
-      node = node->set_next(new SymSeg(sym_length));
+      result = result.push_back({static_cast<unsigned>(sym_length)});
       sym_lengths++;
     }
   }
-  std::shared_ptr<ArgSeg> result(head->next_segment);
-  head->next_segment = nullptr; // prevent double free
   return result;
 }
 
@@ -179,15 +89,9 @@ inline immer::flex_vector<immer::flex_vector<PtrVal>> parse_args(std::string arg
       /* std::cout << "space matched. Skipping" << std::endl; */
       continue;
     }
-    result = result.push_back(match_arg(s)->to_flex_vector().push_back(make_IntV(0, 8)));
+    result = result.push_back(match_result_to_ptrvals(match_arg(s)).push_back(make_IntV(0, 8)));
   } 
   return result;
-  /* return immer::flex_vector<immer::flex_vector<PtrVal>>{ */
-  /*   immer::flex_vector{make_IntV('a', 8), make_IntV('b', 8), make_IntV(0, 8)}, */
-  /*   immer::flex_vector{make_IntV('c', 8), make_IntV('d', 8), make_IntV(0, 8)}, */
-  /* }; */
 }
-
-
 
 #endif
--- dev-clean/headers/test/args_test.cpp
+++ dev-clean/headers/test/args_test.cpp
@@ -1,46 +1,51 @@
 #include <iostream>
 #include <assert.h>
 #include "../llsc.hpp"
+#include <variant>
 
 int main() {
-  std::shared_ptr<ArgSeg> result;
+  using std::variant, std::string;
+  /* immer::flex_vector<unsigned> l1 {1,2,3}; */
+  /* immer::flex_vector<unsigned> l2 {1,2,3}; */
+  MatchResult l1 { {1u}, {"something"} };
+  MatchResult l2 { {1u}, {"something"} };
+  ASSERT((l1 == l2), "equal");
+
+  MatchResult result;
 
   result = match_arg("abc#5def");
-  ASSERT((*result == ConcSeg("abc", new SymSeg(5, new ConcSeg("def")))), 
+  ASSERT((result == MatchResult { {"abc"}, {5u}, {"def"} }),
         "no braces");
 
-  /* std::cout << *result << std::endl; */
-  /* result: [Conc: abc] -> [Sym: 5] -> [Conc: def] */
-
   result = match_arg("#{5}");
-  ASSERT((*result == SymSeg(5)), 
+  ASSERT((result == MatchResult { {5u} }),
         "sym");
 
   result = match_arg("#{5}def");
-  ASSERT((*result == SymSeg(5, new ConcSeg("def"))), 
+  ASSERT((result == MatchResult { {5u}, {"def"} }),
         "sym + conc");
 
   result = match_arg("abc#{5}");
-  ASSERT((*result == ConcSeg("abc", new SymSeg(5))), 
+  ASSERT((result == MatchResult { {"abc"}, {5u} }),
         "conc + sym");
 
   result = match_arg("abc#{5}def");
-  ASSERT((*result == ConcSeg("abc", new SymSeg(5, new ConcSeg("def")))), 
+  ASSERT((result == MatchResult { {"abc"}, {5u}, {"def"} }),
         "conc + sym + conc");
 
   result = match_arg("#{5}abc#{13}");
-  ASSERT((*result == SymSeg(5, new ConcSeg("abc", new SymSeg(13)))),
+  ASSERT((result == MatchResult { {5u}, {"abc"}, {13u} }),
         "sym + conc + sym");
 
   result = match_arg("#{5}#{13}");
-  ASSERT((*result == SymSeg(5, new SymSeg(13))),
+  ASSERT((result == MatchResult { {5u}, {13u} }),
         "sym + sym");
 
   result = match_arg("abc#!{5}def");
-  ASSERT((*result == ConcSeg("abc#{5}def")), 
+  ASSERT((result == MatchResult { {"abc#{5}def"} }),
         "escaping #");
 
   result = match_arg("abc#!!!{5}def");
-  ASSERT((*result == ConcSeg("abc#!{5}def")), 
+  ASSERT((result == MatchResult { {"abc#!{5}def"} }),
         "escaping # and !");
 }
