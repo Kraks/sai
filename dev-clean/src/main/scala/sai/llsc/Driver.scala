18cc2d769032737e79045e42c0b39299f5ddbec6
Songlin <jia137@purdue.edu>
2022-03-05 23:03:53 -0500

    relocate AssignElim

--- dev-clean/src/main/scala/sai/llsc/Driver.scala
+++ dev-clean/src/main/scala/sai/llsc/Driver.scala
@@ -20,45 +20,6 @@ import sai.llsc.imp.ImpCPSLLSCEngine
 
 import sys.process._
 
-object AssignElim {
-  import Backend._
-  type HashSet[A] = collection.mutable.HashSet[A]
-  type Subst = HashMap[Sym, Exp]
-
-  class CollectLookup extends Traverser {
-    val ids = new HashSet[Int]()
-    override def traverse(n: Node): Unit = n match {
-      case Node(s, "ss-lookup-env", StaticList(ss, Const(x: Int)), _) => ids.add(x)
-      case _ => super.traverse(n)
-    }
-  }
-
-  class ElimAssign(val ids: HashSet[Int]) extends Transformer {
-    override val name = "ElimAssign"
-    override def transform(n: Node): Exp = n match {
-      case Node(s, "ss-assign", StaticList(ss: Sym, Const(x: Int), v), _) if !ids.contains(x) =>
-        subst(ss)
-      case _ => super.transform(n)
-    }
-    override def transform(graph: Graph): Graph = {
-      g = Adapter.mkGraphBuilder()
-      Adapter.g = g
-      try {
-        super.transform(graph)
-      } finally {
-        g = null; Adapter.g = null
-      }
-    }
-  }
-
-  def transform(g: Graph): (Graph, Subst) = {
-    val collect = new CollectLookup
-    collect(g)
-    val elim = new ElimAssign(collect.ids)
-    (elim.transform(g), elim.subst)
-  }
-}
-
 abstract class GenericLLSCDriver[A: Manifest, B: Manifest](appName: String, folder: String = ".")
     extends SAISnippet[A, B] with SAIOps { q =>
   import java.io.{File, PrintStream}
