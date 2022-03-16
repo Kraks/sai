fcedc51a58253b34df2f52df51f583ce6ff66edc
Haotian Deng <deng254@xinu01.cs.purdue.edu>
2022-03-16 13:17:37 -0400

    fix test_benchmark and run-experiment

--- dev-clean/run-experiment.sh
+++ dev-clean/run-experiment.sh
@@ -1,14 +1,49 @@
 #!/usr/bin/env bash
 source ~/sai_install/env.sh
 
+rm full.log
+PREGENDIR=/u/antor/u12/deng254/sai_install/coreutils/uclibc_linked_pregen
+MANUALDIR=/u/antor/u12/deng254/sai_install/coreutils/manual_linked
+
 exec >  >(tee -ia full.log)
 exec 2> >(tee -ia full.log >&2)
 
-candidates=(base32 base64 comm cut dirname expand false true fold join link paste pathchk)
+candidates=(
+true
+false
+# base32
+# base64
+# comm
+# cut
+# dirname
+# expand
+# fold
+# join
+# link
+# paste
+# pathchk
+)
+
+declare -A options=(
+[true]='--argv="name #3" --add-sym-file A'
+[false]='--argv="name #3" --add-sym-file A'
+[base32]='--argv="name #3" --add-sym-file A'
+[base64]='--argv="name #3" --add-sym-file A'
+[comm]='--argv="name #3" --add-sym-file A'
+[cut]='--argv="name #3" --add-sym-file A'
+[dirname]='--argv="name #3" --add-sym-file A'
+[expand]='--argv="name #3" --add-sym-file A'
+[fold]='--argv="name #3" --add-sym-file A'
+[join]='--argv="name #3" --add-sym-file A'
+[link]='--argv="name #3" --add-sym-file A'
+[paste]='--argv="name #3" --add-sym-file A'
+[pathchk]='--argv="name #3" --add-sym-file A'
+)
 
 for f in ${candidates[@]}; do
-	fullpath=~/sai_install/coreutils/manual_linked/$f.ll
-	./test_benchmark $fullpath
+	option=${options[$f]}
+	./test_benchmark $PREGENDIR/$f.ll pregen_$f $option
+	./test_benchmark $MANUALDIR/$f.ll manual_$f $option
 done
 
 
