445bc56d0fd2019d3fbeca05c39f2a3ffcfa0642
Guannan Wei <guannanwei@outlook.com>
2022-07-19 01:10:26 -0700

    move getPrimitiveTypeManifest/getPrimitiveTypeName to right place

--- dev-clean/src/main/scala/sai/llsc/ASTUtils.scala
+++ dev-clean/src/main/scala/sai/llsc/ASTUtils.scala
@@ -22,6 +22,34 @@ object ASTUtils {
       List.fill(size)(flattenTy(ety)).flatten
     case _ => List(ty)
   }
+
+  def getPrimitiveTypeManifest(vt: LLVMType): Manifest[_] = vt match {
+    case IntType(1) => manifest[Boolean]
+    case IntType(8) => manifest[Char]
+    case IntType(16) => manifest[Short]
+    case IntType(32) => manifest[Int]
+    case IntType(64) => manifest[Long]
+    case FloatType(FK_Float) => manifest[Float]
+    case FloatType(FK_Double) => manifest[Double]
+    case PtrType(IntType(1), addrSpace) => manifest[Array[Boolean]]
+    case PtrType(IntType(8), addrSpace) => manifest[Array[Char]]
+    case PtrType(IntType(16), addrSpace) => manifest[Array[Short]]
+    case PtrType(IntType(32), addrSpace) => manifest[Array[Int]]
+    case PtrType(IntType(64), addrSpace) => manifest[Array[Long]]
+    case _ => ???
+  }
+
+  def getPrimitiveTypeName(vt: LLVMType): String = vt match {
+    case IntType(1) => "bool"
+    case IntType(8) => "char"
+    case IntType(16) => "short"
+    case IntType(32) => "int"
+    case IntType(64) => "long"
+    case FloatType(FK_Float) => "float"
+    case FloatType(FK_Double) => "double"
+    case PtrType(ty, addrSpace) => "pointer"
+    case _ => ???
+  }
 }
 
 object Constants {
