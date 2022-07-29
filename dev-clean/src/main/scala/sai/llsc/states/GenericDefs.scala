03805d6e5cc3fa8350214eaccbcccbdb4a56cb89
David Deng <daviddenghaotian@163.com>
2022-07-29 16:45:51 -0400

    add back native execution

--- dev-clean/src/main/scala/sai/llsc/states/GenericDefs.scala
+++ dev-clean/src/main/scala/sai/llsc/states/GenericDefs.scala
@@ -154,10 +154,7 @@ trait Opaques { self: SAIOps with BasicDefs =>
           warned.add(id)
         }
         Some(ExternalFun("noop", ret))
-      } else {
-        System.out.println(s"Missing Implementation for ${id}, treated as a noop")
-        Some(ExternalFun("noop", ret))
-      }
+      } else None // Will be executed natively
   }
 }
 
