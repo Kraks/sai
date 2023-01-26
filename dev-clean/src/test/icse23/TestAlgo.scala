707647b52e0c667b5602bfb61f48213901aef89c
Guannan Wei <guannanwei@outlook.com>
2023-01-25 21:09:53 -0500

    add test case for icse23

---
+++ dev-clean/src/test/icse23/TestAlgo.scala
@@ -0,0 +1,36 @@
+package icse23
+
+/* This file is used to test the algorithm benchmarks for
+   ICSE 23 paper "Compiling Parallel Symbolic Execution with Continuations".
+   The ICSE 23 paper presents GenSym, which is the successor of LLSC.
+   See https://github.com/Generative-Program-Analysis/GenSym for more details.
+*/
+
+import sai.llsc._
+
+import sai.lang.llvm._
+import sai.lang.llvm.IR._
+import sai.lang.llvm.parser.Parser._
+
+import lms.core._
+import lms.core.Backend._
+import lms.core.utils.time
+import lms.core.virtualize
+import lms.macros.SourceContext
+import lms.core.stub.{While => _, _}
+
+import sai.lmsx._
+import scala.collection.immutable.{List => StaticList}
+
+import sys.process._
+import org.scalatest.FunSuite
+
+class TestAlgo extends FunSuite {
+    RunLLSC.runLLSC(parseFile("/icse23/GenSym/benchmarks/icse23/algorithms/kmpmatcher.ll"), "kmp", "@main")
+    RunLLSC.runLLSC(parseFile("/icse23/GenSym/benchmarks/icse23/algorithms/mergesort.ll"), "mergeSort", "@main")
+    RunLLSC.runLLSC(parseFile("/icse23/GenSym/benchmarks/icse23/algorithms/bubblesort.ll"), "bubbleSort", "@main")
+    RunLLSC.runLLSC(parseFile("/icse23/GenSym/benchmarks/icse23/algorithms/knapsack.ll"), "knapsack", "@main")
+    RunLLSC.runLLSC(parseFile("/icse23/GenSym/benchmarks/icse23/algorithms/nqueen.ll"), "nqueen", "@main")
+    RunLLSC.runLLSC(parseFile("/icse23/GenSym/benchmarks/icse23/algorithms/quicksort.ll"), "quicksort", "@main")
+}
+
